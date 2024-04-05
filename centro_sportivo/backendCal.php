7c57115c729de204d23b31ef5830d44029095d28
StefanoPezzetta <136790739+StefanoPezzetta@users.noreply.github.com>
2024-04-05 12:50:33 +0200

    Add files via upload

--- centro_sportivo/backendCal.php
+++ centro_sportivo/backendCal.php
@@ -1,17 +1,55 @@
 <?php
-// Retrieve the raw POST data
-$jsonData = file_get_contents('php://input');
-// Decode the JSON data into a PHP associative array
-$data = json_decode($jsonData, true);
-// Check if decoding was successful
-if ($data !== null) {
-   // Access the data and perform operations
-   $anno = $data['anno'];
-   $mese = $data['mese'];
-   $giorno = $data['giorno'];
-   // Perform further processing or respond to the request
+header("Access-Control-Allow-Origin: *");
+
+// Consenti l'utilizzo di alcune intestazioni specifiche (incluso Content-Type)
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Altri header che potrebbero essere richiesti
+
+header('Content-Type: application/json');
+
+require("config.php"); //parametri di connessione
+$mydb = new mysqli(SERVER, UTENTE, PASSWORD, DATABASE);
+if ($mydb->connect_errno) {
+    echo "Errore nella connessione a MySQL: (" . $mydb->connect_errno . ") " . $mydb->connect_error;
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+   // Leggi il corpo della richiesta
+   $json_data = file_get_contents("php://input");
+
+   // Decodifica la stringa JSON in un array associativo
+   $data = json_decode($json_data, true);
+   if ($data !== null) {
+      $anno = $data['anno'];
+      $mese = $data['mese'];
+      $giorno = $data['giorno'];
+      $giornoPr = "$anno-$mese-$giorno";
+
+      $stmt = $mydb->prepare("SELECT campo.nome, prenotazione.ora_inizio, prenotazione.ora_fine 
+      FROM prenotazione join campo on prenotazione.fkCampo = campo.id 
+      where prenotazione.giorno = ?");
+      $stmt->bind_param("s", $giornoPr);
+      if ($stmt->execute()) {
+         $stmt->bind_result($nomeCampo, $oraInizio, $oraFine);
+         while ($stmt->fetch()) {
+             $result[] = [
+                 "nomeCampo" => $nomeCampo,
+                 "oraInizio" => $oraInizio,
+                 "oraFine" => $oraFine,
+             ];
+         }
+         $stmt->close();
+      }else {
+         // Gestione degli errori durante l'esecuzione della query
+         $result = [
+             "error" => "Errore durante l'esecuzione della query"
+         ];
+     }
 } else {
-   // JSON decoding failed
-   http_response_code(400); // Bad Request
+   http_response_code(400);
    echo "Invalid JSON data";
+}
 }
